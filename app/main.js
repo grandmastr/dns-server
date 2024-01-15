6733c0dbaa43eb275a9390285b1cbef2265598be
Israel Akintunde <israelakintunde005@gmail.com>
2024-01-15 12:10:01 +0100

    chore

--- app/main.js
+++ app/main.js
@@ -2,7 +2,32 @@ const dgram = require("dgram");
 const {createDnsSection} = require("./utils/dnsSection");
 
 const udpSocket = dgram.createSocket("udp4");
-udpSocket.bind(2053, "127.0.0.1");
+udpSocket.bind(2054, "127.0.0.1");
+
+const dnsQuery = Buffer.from([
+    0x00, 0x00, // [0-1] Transaction ID
+    0x01, 0x00, // [2-3] Flags
+    0x00, 0x01, // [4-5] Questions
+    0x00, 0x00, // [6-7] Answer RRs
+    0x00, 0x00, // [8-9] Authority RRs
+    0x00, 0x00, // [10-11] Additional RRs
+    // Question
+    0x03, 0x77, 0x77, 0x77, // "www"
+    0x07, 0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, // "example"
+    0x03, 0x63, 0x6f, 0x6d, // "com"
+    0x00, // Null terminator of the domain name
+    0x00, 0x01, // Type A query (Host Address)
+    0x00, 0x01  // Class IN (Internet)
+]);
+
+udpSocket.send(dnsQuery, 2053, "127.0.0.1", (error) => {
+    if (error) {
+        console.error('Error:', error)
+        client.close();
+    } else {
+        console.log('DNS query sent', dnsQuery);
+    }
+})
 
 udpSocket.on("message", (buf, rinfo) => {
     try {
