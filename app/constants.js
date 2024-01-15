d7f31548bc560d85bf8369b8749d346f1c2e306f
Israel Akintunde <israelakintunde005@gmail.com>
2024-01-15 18:23:48 +0100

    handled answer builder

--- app/constants.js
+++ app/constants.js
@@ -1,39 +1,3 @@
-const {createDnsSection} = require("./utils/dnsSections");
-
-const DOMAIN_NAME = 'codecrafters.io';
-
-const defaultHeaderParams = {
-    id: 1,
-    qr: 1,
-    opcode: 0,
-    aa: 0,
-    tc: 0,
-    rd: 1,
-    ra: 1,
-    z: 0,
-    rcode: 0,
-    qdcount: 1,
-    ancount: 1,
-    nscount: 0,
-    arcount: 0
-}
-const dnsQuestionBuffer = createDnsSection({
-    section: 'question',
-    domain_name: DOMAIN_NAME,
-    type: 1,
-    class: 1,
-});
-
-const dnsAnswerBuffer = createDnsSection({
-    section: 'answer',
-    domain_name: DOMAIN_NAME,
-    type: 1,
-    class: 1,
-    ttl: 60,
-    length: 4,
-    data: "8.8.8.8"
-});
-
 module.exports = {
     HEADER: 12,
     dnsQuestionBuffer,
